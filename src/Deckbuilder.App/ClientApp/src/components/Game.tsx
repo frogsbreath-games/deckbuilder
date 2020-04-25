67cfac6bd5e7b6f0fb922e1a29aa6a1be50966f7
Pierce Lauring <pclauring@gmail.com>
2020-04-25 17:58:18 -0400

    Removed template code

---
+++ src/Deckbuilder.App/ClientApp/src/components/Game.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+const Game = () => (
+  <div>
+    <h1>Layout</h1>
+    <p>Test Layout will go here</p>
+  </div>
+);
+
+export default connect()(Game);
