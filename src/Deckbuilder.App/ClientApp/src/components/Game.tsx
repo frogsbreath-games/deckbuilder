ad85f9be2852852e8db94b0ce68cdb72b6b84116
Pierce Lauring <pclauring@gmail.com>
2020-04-28 18:54:26 -0400

    More board setup

--- src/Deckbuilder.App/ClientApp/src/components/Game.tsx
+++ src/Deckbuilder.App/ClientApp/src/components/Game.tsx
@@ -18,9 +18,26 @@ class FetchData extends React.PureComponent<GameProps> {
     return (
       <React.Fragment>
         <div className={styles.board}>
-          <div>Opponent Section</div>
-          <div>Neutral Section</div>
-          <div className={styles.playerSection}>Player Section</div>
+          <div className={styles.opponentSection}>
+            <div className={styles.permanentArea}>Permanent Area</div>
+            <div className={styles.playArea}>Play Area</div>
+            <div className={styles.heroArea}>Hero Area</div>
+            <div className={styles.handArea}>Hand Area</div>
+            <div className={styles.deckArea}>Deck Area</div>
+            <div className={styles.counterArea}>Counter Area</div>
+          </div>
+          <div className={styles.neutralSection}>
+            <div className={styles.storeArea}>Store Area</div>
+            <div className={styles.utilityArea}>Utility Area</div>
+          </div>
+          <div className={styles.playerSection}>
+            <div className={styles.permanentArea}>Permanent Area</div>
+            <div className={styles.playArea}>Play Area</div>
+            <div className={styles.heroArea}>Hero Area</div>
+            <div className={styles.handArea}>Hand Area</div>
+            <div className={styles.deckArea}>Deck Area</div>
+            <div className={styles.counterArea}>Counter Area</div>
+          </div>
         </div>
       </React.Fragment>
     );
