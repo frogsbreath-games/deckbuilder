71dee62d2342f734387ee362aba2a461124b88ce
Benjamin Chodos <bdchodos@gmail.com>
2020-12-05 16:56:14 -0500

    Updated to use craco for webpack overrides

--- src/Deckbuilder.Designer/ClientApp/src/App.tsx
+++ src/Deckbuilder.Designer/ClientApp/src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
-import FetchCards from './components/FetchCards';
+import Layout from '@components/Layout';
+import Home from '@components/Home';
+import Counter from '@components/Counter';
+import FetchData from '@components/FetchData';
+import FetchCards from '@components/FetchCards';
 
 import './custom.css'
 
